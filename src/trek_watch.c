f9bccd5b13979c1daceaa99ff22d172fe3242dbc
Timothy Stough <stough@ginkoleaf.net>
2013-04-14 02:42:28 -0700

    Modified watch face to add month.day to the date line, remove leading zero from hour when in 12h time, and add am/pm text below time.

--- src/trek_watch.c
+++ src/trek_watch.c
@@ -14,15 +14,17 @@ BmpContainer background_image;
 
 TextLayer text_date_layer;
 TextLayer text_time_layer;
+TextLayer text_ampm_layer;
 
 void update_display(PblTm *tick_time) {
-  static char date_text[] = "Xxx 00";
-  static char new_date_text[] = "Xxx 00";
+  static char date_text[] = "Xxx 00.00";
+  static char new_date_text[] = "Xxx 00.00";
   static char time_text[] = "00:00";
+  static char ampm_text[] = "  ";
   char *time_format;
 
   // Draws only if changed, saves your precious battery!
-  string_format_time(new_date_text, sizeof(date_text), "%a %e", tick_time);
+  string_format_time(new_date_text, sizeof(date_text), "%a %m.%d", tick_time);
   if (strncmp(new_date_text, date_text, sizeof(date_text)) != 0) {
       strncpy(date_text, new_date_text, sizeof(date_text));
       text_layer_set_text(&text_date_layer, date_text);
@@ -35,7 +37,23 @@ void update_display(PblTm *tick_time) {
   }
 
   string_format_time(time_text, sizeof(time_text), time_format, tick_time);
+  // Kludge to handle lack of non-padded hour format string
+  // for twelve hour clock.
+  if (!clock_is_24h_style() && (time_text[0] == '0')) {
+    memmove(time_text, &time_text[1], sizeof(time_text) - 1);
+  }
   text_layer_set_text(&text_time_layer, time_text);
+
+  // Write the AM or PM
+  if (!clock_is_24h_style()) {
+    string_format_time(ampm_text, sizeof(ampm_text), "%p", tick_time);
+    if (ampm_text[0] == 'A') {
+      strncpy(ampm_text, "am", sizeof("am"));
+    } else {
+      strncpy(ampm_text, "pm", sizeof("pm"));
+    }
+  }
+  text_layer_set_text(&text_ampm_layer, ampm_text);  
 }
 
 void handle_minute_tick(AppContextRef ctx, PebbleTickEvent *t) {
@@ -55,19 +73,29 @@ void handle_init(AppContextRef ctx) {
   bmp_init_container(RESOURCE_ID_IMAGE_BACKGROUND, &background_image);
   layer_add_child(&window.layer, &background_image.layer.layer);
 
+  // Date Layer
   text_layer_init(&text_date_layer, window.layer.frame);
   text_layer_set_text_color(&text_date_layer, GColorWhite);
   text_layer_set_background_color(&text_date_layer, GColorClear);
-  layer_set_frame(&text_date_layer.layer, GRect(92, 5, 144-92, 168-5));
+  layer_set_frame(&text_date_layer.layer, GRect(78, 5, 144-78, 168-5));
   text_layer_set_font(&text_date_layer, fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_LCARS_BOLD_17)));
   layer_add_child(&window.layer, &text_date_layer.layer);
 
+  // Time Layer
   text_layer_init(&text_time_layer, window.layer.frame);
   text_layer_set_text_color(&text_time_layer, GColorWhite);
   text_layer_set_background_color(&text_time_layer, GColorClear);
   layer_set_frame(&text_time_layer.layer, GRect(40, 48, 144-40, 168-48));
   text_layer_set_font(&text_time_layer, fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_LCARS_60)));
   layer_add_child(&window.layer, &text_time_layer.layer);
+
+  // AMPM Layer
+  text_layer_init(&text_ampm_layer, window.layer.frame);
+  text_layer_set_text_color(&text_ampm_layer, GColorWhite);
+  text_layer_set_background_color(&text_ampm_layer, GColorClear);
+  layer_set_frame(&text_ampm_layer.layer, GRect(40, 105, 144-40, 168-105));
+  text_layer_set_font(&text_ampm_layer, fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_LCARS_BOLD_17)));
+  layer_add_child(&window.layer, &text_ampm_layer.layer);
 
   // No blank screen on init.
   PblTm tick_time;
